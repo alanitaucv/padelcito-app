3ebda5226035b3f6f6db403ae16daf1589ce402c
Zilerimar Fernandez <alanitaucv@gmail.com>
2024-03-31 21:08:30 -0300

    se calculan los partidos bien

--- src/components/RegisterCouples.jsx
+++ src/components/RegisterCouples.jsx
@@ -41,7 +41,7 @@ export const RegisterCouples = ({ couples, time }) => {
         setPlayers(players);
     }
 
-    console.log(players);
+   // console.log(players);
     return (
         <>
             <div>
