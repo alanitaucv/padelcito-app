26d4a773b8bcd1486e81722ccde396be7959c449
Zilerimar Fernandez <alanitaucv@gmail.com>
2024-03-31 22:19:45 -0300

    Funcion que calcula los partidos

---
+++ src/components/Matches.jsx
@@ -0,0 +1,23 @@
+import {organizarPartidos } from "../helpers/CreateMatches";
+
+export const Matches = (  { couples, time } ) => {
+
+    const rounds = organizarPartidos(couples);
+    console.log("rondas", rounds);
+    
+    const warmingUpTime = 5;
+    const roundsNumber = rounds.length;
+    let timeOnCourt = (time - warmingUpTime) ;
+
+    return (
+    <>
+        <h3>Partidos</h3>
+        <h4>{ roundsNumber } Rondas. { couples.length/2 } Canchas.</h4>
+        <h5>{ (Math.floor(timeOnCourt/roundsNumber)) } minutos por partido y { 5 }min. de calentamiento.</h5>
+        {
+            rounds.map((r, index) => (
+                <p key={index}>{r}</p>
+            ))
+        }
+    </>); 
+}
---
+++ src/helpers/CreateMatches.js
@@ -0,0 +1,33 @@
+export const organizarPartidos = (couples) => {
+        
+    const justIds = couples.map((x) => x.id);
+
+    const sizeGroups = justIds.length/2;
+    const sizeTeams = justIds.length;
+    const groupTeamsSize = sizeGroups;
+    
+    let group1 = justIds.slice(0, sizeGroups)
+    let group2 = justIds.slice( sizeGroups, sizeTeams)
+    
+    let rounds = [];
+    rounds[0] = [group1+ "," + group2];
+
+    //Este ciclo implementa el algoritmo de arreglos circulares dejando el primer elemento del primer arreglo fijo        
+    for (let r = 1; r < sizeTeams - 1; r++){
+        const lastElement = group1[groupTeamsSize - 1];
+        const firtsElementG2 = group2[0];
+
+        for (let i = 0; i < groupTeamsSize-1; i++){
+            if (i!=0) group1[i+1] = group1[i];
+        }
+        group1[1] = firtsElementG2;
+
+        for (let j = 0; j < groupTeamsSize - 1; j++) {
+            group2[j] = group2[j+1];
+        }
+        group2[groupTeamsSize-1] = lastElement;
+        rounds[r] = [group1+ "," + group2];
+    }
+    
+    return rounds;
+}
